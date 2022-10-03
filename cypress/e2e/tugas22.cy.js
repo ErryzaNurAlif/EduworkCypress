a667c58e6ea2a1cea8f9d22360bd7f7e369adc37
Erryza Nur Alif <erryzanuralif@gmail.com>
2022-10-03 15:35:33 +0700

    Tugas 22 Materi Test Script

---
+++ cypress/e2e/tugas22.cy.js
@@ -0,0 +1,15 @@
+describe("My First Test", () => {
+  it('clicking "type" shows the right headings', () => {
+    cy.visit("http://example.cypress.io");
+
+    cy.pause();
+
+    cy.contains("type").click();
+
+    cy.url().should("include", "/commands/actions");
+
+    cy.get(".action-email")
+      .type("test@gmail.com")
+      .should("have.value", "test@gmail.com");
+  });
+});
